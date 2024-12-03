ce3295e631b231cd7d82de6138e4af167200c82d
swirly-way <s4m14z1l@gmail.com>
2024-12-03 16:44:18 +0000

    Complete ticket 3

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ console.log('Hello World!');
 
 let person = {
     name: 'Sami',
+    age: 28,
     eyeColour: "brown",
     favFood: "lasagna",
     height: "185cm",
     favSong: "baby shark",
 }
 
+let person2 = {
+    name: 'Susan',
+    age: 24,
+    eyeColour: "brown",
+    favFood: "lasagna",
+    height: "185cm",
+    favSong: "baby shark",
+}
+
+let person3 = {
+    name: 'Silvio',
+    age: 22,
+    eyeColour: "brown",
+    favFood: "lasagna",
+    height: "185cm",
+    favSong: "baby shark",
+}
+
+console.log(person.age, person2.age, person3.age);
+person.age += 1; 
+console.log(person.age);
